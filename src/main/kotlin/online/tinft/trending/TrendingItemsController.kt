808d3b41de5839cc6a9d81691ae79567e31fcba7
egorstreshnev <e.streshnev@step.app>
2023-03-06 21:25:12 +0300

    Added trending collections + trending items requests

--- src/main/kotlin/online/tinft/trending/TrendingItemsController.kt
+++ src/main/kotlin/online/tinft/trending/TrendingItemsController.kt
@@ -4,21 +4,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
-suspend fun getTrendingItems(httpClient: HttpClient): List<TrendingItem> {
-    return getTrendingCollectionsME(httpClient)
-        .flatMap {
-            getTrendingItemsByCollectionME(httpClient, it.collectionSymbol).results
-        }
-        .map {
-            TrendingItem(
-                id = it.mintAddress,
-                name = it.title,
-                image = it.img,
-                price = it.price,
-                likesCount = (10..100).random(),
-            )
-        }
-        .sortedByDescending { it.likesCount }
+suspend fun getTrendingItems(httpClient: HttpClient): String {
+    return getTrendingItemsByCollectionME(httpClient, "y00ts")
 }
 
 private suspend fun getTrendingCollectionsME(httpClient: HttpClient): List<TrendingCollectionME> {
@@ -28,7 +15,7 @@ private suspend fun getTrendingCollectionsME(httpClient: HttpClient): List<Trend
     return items.body()
 }
 
-private suspend fun getTrendingItemsByCollectionME(httpClient: HttpClient, collectionId: String): TrendingItemResultME {
+private suspend fun getTrendingItemsByCollectionME(httpClient: HttpClient, collectionId: String): String {
     val items = httpClient.get(
         "https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=$collectionId&direction=2&field=1&limit=6&offset=0&mode=all",
     )
