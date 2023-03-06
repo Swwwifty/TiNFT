1ec70abb16be21f1e30a7808d3e72c5b06fb0075
egorstreshnev <e.streshnev@step.app>
2023-03-06 14:33:13 +0300

    make it better

--- src/main/kotlin/online/tinft/Application.kt
+++ src/main/kotlin/online/tinft/Application.kt
@@ -6,14 +6,14 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
-import io.ktor.server.tomcat.Tomcat
+import io.ktor.server.netty.Netty
 import kotlinx.serialization.json.Json
 import online.tinft.plugins.configureHTTP
 import online.tinft.plugins.configureRouting
 import online.tinft.plugins.configureSerialization
 
 fun main() {
-    embeddedServer(Tomcat, port = System.getenv("PORT").toInt(), host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), module = Application::module)
         .start(wait = true)
 }
 
