79f33d1e95ef936a20d0e2a8c4b32bd34626c768
egorstreshnev <e.streshnev@step.app>
2023-03-09 16:25:11 +0300

    Added trending collections + trending items requests

--- src/main/kotlin/online/tinft/collections/CollectionsController.kt
+++ src/main/kotlin/online/tinft/collections/CollectionsController.kt
@@ -3,8 +3,18 @@ package online.tinft.collections
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import java.util.Calendar
+
+private val cache = mutableListOf<CollectionResponse>()
+private var cacheTime: Long = 0
 
 suspend fun getCollections(httpClient: HttpClient): List<CollectionResponse> {
+    val curTime = Calendar.getInstance().time.time
+    if (cache.isNotEmpty() && curTime - cacheTime < 30 * 60 * 1000) {
+        return cache
+    }
+    cacheTime = curTime
+    cache.clear()
     return getCollectionsME(httpClient).map {
         CollectionResponse(
             id = it.collectionSymbol,
@@ -14,6 +24,8 @@ suspend fun getCollections(httpClient: HttpClient): List<CollectionResponse> {
             volume1d = it.vol,
             floorPrice = it.fp,
         )
+    }.also {
+        cache.addAll(it)
     }
 }
 
