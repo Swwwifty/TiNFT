8026b916c99ffc28d5917b9fea21022922ed6568
egorstreshnev <e.streshnev@step.app>
2023-03-12 17:48:39 +0300

    Added trending collections + trending items requests

--- src/main/kotlin/online/tinft/plugins/ConfigureRouting.kt
+++ src/main/kotlin/online/tinft/plugins/ConfigureRouting.kt
@@ -3,6 +3,10 @@ package online.tinft.plugins
 import io.ktor.client.HttpClient
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.http.content.default
+import io.ktor.server.http.content.files
+import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticRootFolder
 import io.ktor.server.plugins.autohead.AutoHeadResponse
 import io.ktor.server.resources.Resources
 import io.ktor.server.routing.routing
@@ -10,6 +14,7 @@ import online.tinft.collections.collectionsRouting
 import online.tinft.items.itemsRouting
 import online.tinft.payment.paymentRouting
 import online.tinft.trending.trendingRouting
+import java.io.File
 
 fun Application.configureRouting(httpClient: HttpClient) {
     install(Resources)
@@ -19,5 +24,10 @@ fun Application.configureRouting(httpClient: HttpClient) {
         trendingRouting(httpClient)
         itemsRouting(httpClient)
         paymentRouting(httpClient)
+        static("/") {
+            staticRootFolder = File("files")
+            files(".")
+            default("index.html")
+        }
     }
 }
